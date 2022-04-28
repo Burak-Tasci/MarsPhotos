59adb85ada6effe6bf4abffb449f2a80e1dac0b2
Burak-Tasci <buraaktascii@outlook.com>
2022-04-28 21:38:51 +0300

    update | api key location

---
+++ app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.tsci.marsphotostask.common
+
+object Constants {
+
+    const val BASE_URL: String = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
+    const val API_KEY: String = "WM6k2IFbd7OQlpeAXzKS19wgxM5WP03zEvh7ziRH"
+}
