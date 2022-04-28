aac529d8a76779df4085b9218490d417d611e84b
Burak-Tasci <buraaktascii@outlook.com>
2022-04-29 00:19:00 +0300

    add | enum class to be able to handle api requests

--- app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
+++ app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val BASE_URL: String = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
     const val API_KEY: String = "WM6k2IFbd7OQlpeAXzKS19wgxM5WP03zEvh7ziRH"
+
+    enum class Rovers { CURIOUSITY, OPPORTUNITY, SPIRIT }
 }
