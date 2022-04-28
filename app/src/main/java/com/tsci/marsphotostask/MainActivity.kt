89b6f44da7d7b8a14a83ad586326355b53375e51
Burak-Tasci <buraaktascii@outlook.com>
2022-04-28 18:58:42 +0300

    Initial commit
    Initial commit also has a few updates on layout files :(

---
+++ app/src/main/java/com/tsci/marsphotostask/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.tsci.marsphotostask
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.tsci.marsphotostask.ui.main.SectionsPagerAdapter
+import com.tsci.marsphotostask.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+    }
+}
