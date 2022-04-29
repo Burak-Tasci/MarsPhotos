4a975c56876dd18c74724fa71fffdd212e0ca306
Burak-Tasci <buraaktascii@outlook.com>
2022-04-30 01:25:22 +0300

    update | default sol value

--- app/src/main/java/com/tsci/marsphotostask/data/remote/MarsPhotoApi.kt
+++ app/src/main/java/com/tsci/marsphotostask/data/remote/MarsPhotoApi.kt
@@ -9,19 +9,19 @@ interface MarsPhotoApi {
 
     @GET("curiosity/photos")
     suspend fun getCuriosityMarsPhotos(
-        @Query("sol") sol:Int = 1000,
+        @Query("sol") sol:Int = 1900,
         @Query("page") page: Int,
         @Query("api_key") api_key: String = API_KEY
     ): ResponseApi
     @GET("spirit/photos")
     suspend fun getSpiritMarsPhotos(
-        @Query("sol") sol:Int = 1000,
+        @Query("sol") sol:Int = 1900,
         @Query("page") page: Int,
         @Query("api_key") api_key: String = API_KEY
     ): ResponseApi
     @GET("opportunity/photos")
     suspend fun getOpportunityMarsPhotos(
-        @Query("sol") sol:Int = 1000,
+        @Query("sol") sol:Int = 1900,
         @Query("page") page: Int,
         @Query("api_key") api_key: String = API_KEY
     ): ResponseApi
