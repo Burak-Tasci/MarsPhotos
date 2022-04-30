811b9b69eb729e1b36f99026b9cf3cc3888c65f3
Burak-Tasci <buraaktascii@outlook.com>
2022-04-30 22:45:48 +0300

    create | custom dialog screen

---
+++ app/src/main/java/com/tsci/marsphotostask/presentation/ui/CustomDialog.kt
@@ -0,0 +1,67 @@
+package com.tsci.marsphotostask.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import coil.load
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.ShapeAppearanceModel
+import com.tsci.marsphotostask.R
+import com.tsci.marsphotostask.databinding.MarsphotoItemBinding
+import com.tsci.marsphotostask.domain.model.MarsPhoto
+
+class CustomDialog(
+    private val item: MarsPhoto
+): DialogFragment(R.layout.marsphoto_item) {
+
+    private var _binding: MarsphotoItemBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.round_corner);
+        _binding = MarsphotoItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
+        dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.run {
+
+            photo.load(
+                item.imgSrc
+            ){
+                placeholder(R.drawable.loading_animation)
+                error(R.drawable.ic_baseline_broken_image_24)
+            }
+            val shapeAppearanceModel = ShapeAppearanceModel()
+                .toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED,
+                    resources.getDimension(R.dimen.corner_radius)
+                )
+                .build()
+
+            photo.shapeAppearanceModel = shapeAppearanceModel
+
+            roverName.text = item.roverName
+            earthDate.text = item.earthDate
+            cameraName.text = resources.getString(R.string.camera_name, item.cameraName)
+            landingDate.text = resources.getString(R.string.landing_date, item.landingDate)
+            launchDate.text = resources.getString(R.string.launch_date, item.launchDate)
+            missionStatus.text = resources.getString(R.string.mission_status, item.status)
+
+        }
+    }
+
+
+}
