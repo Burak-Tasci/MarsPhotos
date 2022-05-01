8ba5f43c7a71cc5b4a990378d5e2cc1a2520df9c
Burak-Tasci <buraaktascii@outlook.com>
2022-05-01 20:56:23 +0300

    update | method parameter and pagination configuration

--- app/src/main/java/com/tsci/marsphotostask/presentation/PageViewModel.kt
+++ app/src/main/java/com/tsci/marsphotostask/presentation/PageViewModel.kt
@@ -9,7 +9,10 @@ import androidx.paging.cachedIn
 import com.tsci.marsphotostask.common.Constants
 import com.tsci.marsphotostask.data.paging.MarsPhotoPagingSource
 import com.tsci.marsphotostask.data.remote.MarsPhotoApi
+import com.tsci.marsphotostask.data.remote.dto.Photo
+import com.tsci.marsphotostask.data.remote.dto.toMarsPhoto
 import com.tsci.marsphotostask.domain.model.MarsPhoto
+import com.tsci.marsphotostask.domain.repository.MarsPhotoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -18,14 +21,16 @@ private const val TAG = "PageViewModel.kt"
 
 @HiltViewModel
 class PageViewModel @Inject constructor(
-    private val apiService: MarsPhotoApi
+    private val repository: MarsPhotoRepository
 ) : ViewModel() {
 
+    val listData = MutableLiveData<List<MarsPhoto>>()
 
-    internal fun getPhotos(rover: Constants.Rovers): Flow<PagingData<MarsPhoto>> {
-        Log.d(TAG, "getPhotos: $rover")
-        return Pager(PagingConfig(pageSize = 25)) {
-            MarsPhotoPagingSource(apiService, rover = rover)
+    internal fun getPhotos(roverName: String): Flow<PagingData<MarsPhoto>> {
+        return  Pager(PagingConfig(pageSize = 1,
+            initialLoadSize = 1,
+            prefetchDistance = 1)) {
+            MarsPhotoPagingSource(repository, rover = roverName)
         }.flow.cachedIn(viewModelScope)
     }
 }
