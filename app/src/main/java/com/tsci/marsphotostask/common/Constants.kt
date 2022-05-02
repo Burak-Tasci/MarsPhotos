bb60314e7ba8a2e74a31f6f88abfb23520249543
Burak-Tasci <buraaktascii@outlook.com>
2022-05-02 13:11:32 +0300

    add | sol constant

--- app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
+++ app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
@@ -3,7 +3,12 @@ package com.tsci.marsphotostask.common
 object Constants {
 
     const val BASE_URL: String = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
-    const val API_KEY: String = "WM6k2IFbd7OQlpeAXzKS19wgxM5WP03zEvh7ziRH"
+    const val API_KEY: String = "Dz67ki2ScBpsrxLC0W2CdOxZDUDLcj0te5FSVzgk"
+    const val SOL: Int = 1000
 
-    enum class Rovers { CURIOUSITY, OPPORTUNITY, SPIRIT }
+//    WM6k2IFbd7OQlpeAXzKS19wgxM5WP03zEvh7ziRH
+//    HmsWY2dljqU2cUh2aCcxO49aSFgfpS7T8wFgSfX0
+//    Dz67ki2ScBpsrxLC0W2CdOxZDUDLcj0te5FSVzgk
+//    uvfxlZTSwBjxhWfQ9i3ZgPGoq5bitqAfa9BLZVfk
+    enum class Rovers { CURIOSITY, OPPORTUNITY, SPIRIT }
 }
