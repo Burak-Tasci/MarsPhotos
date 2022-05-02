dff72c69823d57a652361431594053a70519cd3b
Burak-Tasci <buraaktascii@outlook.com>
2022-05-03 02:48:56 +0300

    update | constants

--- app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
+++ app/src/main/java/com/tsci/marsphotostask/common/Constants.kt
@@ -3,7 +3,7 @@ package com.tsci.marsphotostask.common
 object Constants {
 
     const val BASE_URL: String = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
-    const val API_KEY: String = "uvfxlZTSwBjxhWfQ9i3ZgPGoq5bitqAfa9BLZVfk"
+    const val API_KEY: String = "WM6k2IFbd7OQlpeAXzKS19wgxM5WP03zEvh7ziRH"
     const val SOL: Int = 1000
 
 //    WM6k2IFbd7OQlpeAXzKS19wgxM5WP03zEvh7ziRH
